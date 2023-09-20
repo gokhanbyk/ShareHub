a8e0cd6727f02c985d3835a6f0002364fdd26c5a
Gökhan Bıyıkoğlu <gokhan_byk@icloud.com>
2023-09-20 22:45:18 +0300

    end of the day

--- user/views.py
+++ user/views.py
@@ -16,7 +16,7 @@ def show_user_fav_view(request):
     
     
     context = dict(
-        title = "Favorilerim",
+        title = "Favourites",
         favs=BlogPost.objects.filter(id__in=ids, is_active=True)
     )    
 
