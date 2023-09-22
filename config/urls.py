66cd149de25350848ad7dc6504491b1b5f48fb35
Gökhan Bıyıkoğlu <gokhan_byk@icloud.com>
2023-09-22 16:25:18 +0300

    staticfiles

--- config/urls.py
+++ config/urls.py
@@ -38,4 +38,5 @@ urlpatterns = [
     path('blog/', include('blog.urls', namespace='blog')),
 
     path('admin/', admin.site.urls),
-] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT) \
++ static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
